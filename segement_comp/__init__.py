eda36405d25ef2faa2249abcf80b3c86b0e5eb65
bert <bertcug@gmail.com>
2016-03-04 09:44:34 +0800

    添加类型映射

---
+++ segement_comp/__init__.py
@@ -0,0 +1,17 @@
+#coding=utf-8
+
+import sys
+sys.path.append("..")
+
+from algorithm.ast import get_function_ast_root, serializedAST
+
+def get_type_mapping_table(neo4j_db, func_name):
+    ast_root = get_function_ast_root(neo4j_db, func_name)
+    if ast_root is None:
+        print u"节点不存在"
+    
+    ser = serializedAST(neo4j_db)
+    ser.genSerilizedAST(ast_root)
+    
+    return ser.variable_maps
+    
--- segement_comp/ast_segement.py
+++ segement_comp/ast_segement.py
@@ -10,8 +10,10 @@ import py2neo
 import datetime
 import re
 from algorithm.suffixtree import suffixtree
+from segement_comp import get_type_mapping_table
 
-def segement_ast_similarity_process(vuln_name, patch_name, neo4jdb, worksheet, suffix_tree_obj):
+def segement_ast_similarity_process(vuln_name, patch_name, neo4jdb, type_mapping,
+                                     worksheet, suffix_tree_obj):
     start_time = time.time()
     print "[%s] processing %s" % (datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S"),
                                    vuln_name + " vs " + patch_name)
@@ -30,11 +32,23 @@ def segement_ast_similarity_process(vuln_name, patch_name, neo4jdb, worksheet, s
         worksheet.append(line)
         return
     
+    
+    
     #序列化AST返回值是一个数组，0元素是序列化的AST字符串，1元素是节点个数，AST字符串以;结尾，需要去掉结尾的;
-    pattern1 = serializedAST(neo4jdb, True, True).genSerilizedAST(vuln_func)[0][:-1]
-    pattern2 = serializedAST(neo4jdb, False, True).genSerilizedAST(vuln_func)[0][:-1] 
-    pattern3 = serializedAST(neo4jdb, True, False).genSerilizedAST(vuln_func)[0][:-1]
-    pattern4 = serializedAST(neo4jdb, False, False).genSerilizedAST(vuln_func)[0][:-1]
+    o1 = serializedAST(neo4jdb, True, True)
+    o2 = serializedAST(neo4jdb, False, True)
+    o3 = serializedAST(neo4jdb, True, False)
+    o4 = serializedAST(neo4jdb, False, False)
+   
+    o1.data_type_mapping = type_mapping
+    o2.data_type_mapping = type_mapping
+    o3.data_type_mapping = type_mapping
+    o4.data_type_mapping = type_mapping
+    
+    pattern1 = o1.genSerilizedAST(vuln_func)[0][:-1]
+    pattern2 = o2.genSerilizedAST(vuln_func)[0][:-1] 
+    pattern3 = o3.genSerilizedAST(vuln_func)[0][:-1]
+    pattern4 = o4.genSerilizedAST(vuln_func)[0][:-1]
     
     #delete FunctionDef and CompoundStatement node
     prefix_str = r"^FunctionDef\([0-9]+\);CompoundStatement\([0-9]+\);"
@@ -43,10 +57,10 @@ def segement_ast_similarity_process(vuln_name, patch_name, neo4jdb, worksheet, s
     pattern3 = re.sub(prefix_str, "", pattern3)
     pattern4 = re.sub(prefix_str, "", pattern4)
     
-    s1 = serializedAST(neo4jdb, True, True).genSerilizedAST(patched_func)[0][:-1]
-    s2 = serializedAST(neo4jdb, False, True).genSerilizedAST(patched_func)[0][:-1]
-    s3 = serializedAST(neo4jdb, True, False).genSerilizedAST(patched_func)[0][:-1]
-    s4 = serializedAST(neo4jdb, False, False).genSerilizedAST(patched_func)[0][:-1]
+    s1 = o1.genSerilizedAST(patched_func)[0][:-1]
+    s2 = o2.genSerilizedAST(patched_func)[0][:-1]
+    s3 = o3.genSerilizedAST(patched_func)[0][:-1]
+    s4 = o4.genSerilizedAST(patched_func)[0][:-1]
     
     report = {}
     if suffix_tree_obj.search(s1, pattern1):
@@ -81,9 +95,14 @@ if __name__ == "__main__":
     try:
         neo4jdb = py2neo.Graph("http://127.0.0.1:7499/db/data/")
     except Exception:
-        print "数据库连接失败"
+        print u"数据库连接失败:7499"
+    
+    try:
+        org_db = py2neo.Graph()
+    except Exception:
+        print u"数据库连接失败:7474"
         
-    wb = load_workbook("test3.xlsx", read_only=True)
+    wb = load_workbook("test3.xlsx", guess_types=True)
     ws = wb[u'Sheet3']
     
     workbook = Workbook()
@@ -94,8 +113,16 @@ if __name__ == "__main__":
     
     suffix_tree_obj = suffixtree()
     for row in ws.rows:
+        type_mapping = {'other':'v'}
+        if row[2].value != 0:
+            func_name = row[0].value[:19] + row[2].value
+            type_mapping = get_type_mapping_table(org_db, func_name)
+            row[3].value = type_mapping.__str__()
+            wb.save("test3.xlsx")
+            
         try:
-            segement_ast_similarity_process(row[0].value, row[1].value, neo4jdb, worksheet, suffix_tree_obj)
+            segement_ast_similarity_process(row[0].value, row[1].value, neo4jdb, 
+                                            type_mapping, worksheet, suffix_tree_obj)
             workbook.save("ast_segement_result.xlsx")
         except Exception as e:
             print "process " + row[0].value + "error"
