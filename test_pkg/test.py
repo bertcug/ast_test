c74910a58c99d4708605b721d305407119a96368
bert <bertcug@gmail.com>
2016-03-30 10:27:55 +0800

    测试代码复用

--- test_pkg/test.py
+++ test_pkg/test.py
@@ -24,11 +24,13 @@ def search_vuln_seg_in_patched(db1, vuln_seg, db2, patched_name, suffix_obj, wor
     if vuln_seg_func is None:
         print "%s is not found" % vuln_seg
         worksheet.append( (vuln_seg, patched_name, "vuln_not_found","-", "-","-", "-","-","-") )
+        return
     
     patched_func = get_function_ast_root(db2, patched_name)
     if patched_func is None:
         print "%s is not found" % patched_name
         worksheet.append( (vuln_seg, patched_name, "patch_not_found","-", "-", "-", "-","-","-") )
+        return
     
     o1 = serializedAST(db2, True, True)
     o2 = serializedAST(db2, False, True)
@@ -84,7 +86,7 @@ def search_vuln_seg_in_patched(db1, vuln_seg, db2, patched_name, suffix_obj, wor
                        report["distinct_const_no_type"], report["distinct_type_no_const"],
                        report['no_type_no_const'], match, simi) )
     
-def main():
+def wireshark_diff():
     data = load_workbook("Wireshark.xlsx", read_only=True)[u'Sheet3']
     suffix_obj = suffixtree()
     
@@ -107,8 +109,52 @@ def main():
     suffix_obj.close()
     print "all works done"
 
-if __name__ == "__main__":
-    main()
+def get_segements(segements, patch_func_name):
+    cveid = patch_func_name[0:13]
+    ret = []
+    for segement in segements:
+        if segement.startswith(cveid):
+            ret.append(segement)
+    return ret    
+
+def code_reuse():
+    wb = load_workbook("data.xlsx")
+    
+    wireshark_segement_list = []
+    for row in wb['wireshark'].rows:
+        wireshark_segement_list.append(row[0].value)
+    ffmpeg_segement_list = []
+    for row in wb['ffmpeg'].rows:
+        ffmpeg_segement_list.append(row[0].value)
+    
+    db = Graph("http://127.0.0.1:7476/db/data/")
+    suffix_obj = suffixtree()
+    #wireshark
+    result = Workbook()
+    ws = result.create_sheet("wireshark_test",0)
+    for row in wb['Sheet2'].rows:
+        test_list = get_segements(wireshark_segement_list, row[0].value)
+        for test in test_list:
+            try:
+                search_vuln_seg_in_patched(db, test, db, row[0].value, suffix_obj, ws)
+                result.save("code_reuse.xlsx")
+            except Exception as e:
+                print e
         
+    #ffmpeg
+    ff_ws = result.create_sheet("ffmpeg_test",1)
+    for row in wb['Sheet1'].rows:
+        test_list = get_segements(ffmpeg_segement_list, row[0].value)
+        for test in test_list:
+            try:
+                search_vuln_seg_in_patched(db, test, db, row[0].value, suffix_obj, ws)
+                result.save("code_reuse.xlsx")
+            except Exception as e:
+                print e  
+    suffix_obj.close()
+    print "all works done"
+      
+if __name__ == "__main__":
+    code_reuse()
     
     
