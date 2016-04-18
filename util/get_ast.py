7eff23b7cd84565b86fcb39e3f871682cb404345
bert <bertcug@gmail.com>
2016-04-18 19:07:02 +0800

    2016-4-18 19:06

--- util/get_ast.py
+++ util/get_ast.py
@@ -9,10 +9,11 @@ import argparse
 if __name__ == "__main__":
     parse = argparse.ArgumentParser()
     parse.add_argument("-f", "--func", help="function name")
+    parse.add_argument("-p", "--port", help="db port")
     args = parse.parse_args()
     
     func = args.func
-    db = Graph("http://127.0.0.1:7473/db/data/")
+    db = Graph("http://127.0.0.1:%d/db/data/" % args.port)
     
     ast_root = get_function_ast_root(db, func)
     ser = serializedAST(db)
