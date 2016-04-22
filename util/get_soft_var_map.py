cceae3b068187f8e37401f461f1921b9a5ad5112
bert <bertcug@gmail.com>
2016-04-23 00:13:14 +0800

    sqlite参数化查询全面改造

--- util/get_soft_var_map.py
+++ util/get_soft_var_map.py
@@ -26,7 +26,7 @@ def get_software_var_map(soft, port):
     funcs = get_all_functions(neo4j_db)
     for func in funcs:
         # 查重
-        ret = sql_db.execute("select * from %s where func_id=%d" % (soft, func._id))
+        ret = sql_db.execute("select * from %s where func_id=？" % soft, (func._id,))
         if ret.fetchone():
             continue
         
@@ -36,8 +36,8 @@ def get_software_var_map(soft, port):
         ser.genSerilizedAST(ast_root)
         var_map = ser.variable_maps
         
-        query = 'insert into %s values(%d, "%s", "%s", "%s")' %(soft, func._id,
-            func.properties[u'name'],func_file, var_map.__str__())
+        query = 'insert into %s values(?, ?, ?, ?)' % soft,\
+                (func._id, func.properties[u'name'],func_file, var_map.__str__())
         try:
             sql_db.execute(query)
             sql_db.commit()
